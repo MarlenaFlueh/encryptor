0168f1e75335eb73d0d6eaa46a60ae00a208b658
MarlenaFlueh <mf@esyoil.com>
2017-11-30 14:23:28 +0100

    add shiftChar

--- main.js
+++ main.js
@@ -6,35 +6,37 @@ input.addEventListener('input', event => {
   p.innerHTML = rot13(event.target.value);
 });
 
+// shiftChar(char: string, shift: number): string -> shifts one char
+const shiftChar = (char, shift) => {
+  if (abc.indexOf(char) === -1) {
+    return char;
+  } else {
+    return abc[(abc.indexOf(char) + shift) % abc.length];
+  }
+};
+
+// shiftByValue(message: string, shift: number): string -> shifts a whole message
 const shiftByValue = (message, shift) => {
   let result = '';
   
   for (let i = 0; i < message.length; i++) {
-    if (abc.indexOf(message[i]) === -1) {
-      result += message[i];
-    } else {
-      result += abc[(abc.indexOf(message[i]) + shift) % abc.length];
-    }
+    result += shiftChar(message[i], shift);
   }
   
   return result;
 };
 
+// rot13(message: string): string -> shifts message by 13
 const rot13 = message => {
    return shiftByValue(message, 13);
 };
 
+// vigenere(message: string, key: string): string -> shifts a message by another string
 const vigenere = (message, key) => {
   let result = '';
 
   for (let i = 0; i < message.length; i++) {
-    if (abc.indexOf(message[i]) === -1) {
-      result += message[i];
-    } else {
-      const keyIndex = abc.indexOf(key[i % key.length]);
-      const messageIndex = abc.indexOf(message[i]);
-      result += abc[(messageIndex + keyIndex) % abc.length];
-    }
+    result += shiftChar(message[i], abc.indexOf(key[i % key.length]));
   }
 
   return result;
