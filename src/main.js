8cb7829c13e9d54b5ee779a93d2fe307b08ae88f
lwesy <lw@esyoil.com>
2017-12-02 19:48:38 +0100

    add webpack-dev-server

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { rot13 } from '../lib/encryptor';
 
 const input = document.querySelector('input');
 const p = document.querySelector('p');
-const abc = 'abcdefghijklmnopqrstuvwxyz';
 
 input.addEventListener('input', event => { 
   p.innerHTML = rot13(event.target.value);
